be6f6ef7c05ffec146b7bdb3c169b379691bd9f7
Justin Scott Jenecke <93656319+codespace-justin@users.noreply.github.com>
2022-04-24 21:45:16 +0200

    renames folder and uses foreach loop to populate index page

---
+++ 5-structuring-internally/c-workout-manager/1-memo/src/includes/data/premadePlans.php
@@ -0,0 +1,24 @@
+<?php
+
+// 3D array of plans
+$premadePlans = [
+
+    # array 1
+    "beginner" => [
+        'level' => 'beginner',
+        'days' => 3,
+        'split' => 'Full-Body workouts with low intensity'
+    ],
+    # array 2
+    "intermediate" => [
+        'level' => 'intermediate',
+        'days' => 4,
+        'split' => 'Push excercises, Pull excercises, Leg excercises'
+    ],
+    # array 3
+    'expert' => [
+        'level' => 'expert',
+        'days' => 6,
+        'split' => 'Targeted muscle group every day (1 rest day)'
+    ]
+];
---
+++ 6-forms-and-superglobals/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        li {
+            display: flex;
+            justify-content: space-evenly;
+        }
+    </style>
+</head>
+<body>
+
+    <h1> Users </h1>
+
+    <ul>
+        <li>
+            <span>a</span>
+            <span>x</span>
+            <span>z</span>
+            <span>7</span>
+        </li>
+    </ul>
+
+    <br>
+    <hr>
+    <br>
+
+    <form action="" method="post"></form>
+
+    <br>
+    <hr>
+    <br>
+
+</body>
+</html>
