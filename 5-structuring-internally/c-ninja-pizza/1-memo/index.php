adc45f583a1b585cfbf1954ed6bb69056a8bf6fe
Justin Scott Jenecke <93656319+codespace-justin@users.noreply.github.com>
2022-04-24 19:21:52 +0200

    links bulma cdn, creates empty header and footer templates

--- 5-structuring-internally/c-ninja-pizza/1-memo/index.php
+++ 5-structuring-internally/c-ninja-pizza/1-memo/index.php
@@ -5,11 +5,54 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/css/theme.css">
     <title>Ninja Pizza</title>
 </head>
 
 <body>
+
+    <!-- Header -->
+    <header class="section has-background-danger">
+        <h1 class="title">
+            Ninja Pizza
+        </h1>
+        <nav class="is-flex is-justify-content-space-evenly">
+            <button class="button  px-6">
+                { / };
+            </button>
+            <button class="button  px-6">
+                Order
+            </button>
+            <button class="button  px-6">
+                About
+            </button>
+            <button class="button  px-6">
+                Contact
+            </button>
+        </nav>
+    </header>
+
+
+    <!-- Page -->
+    <main class="section content">
     
+        Ninja Pizza Specials here
+    
+    </main>
+
+
+    <!-- Footer -->
+    <footer class="section has-background-danger columns footer">
+        <p class="column">
+            Email - pizza@ninja.co.za <br>
+            Tel - (021) 906 3454 <br>
+            IG - pizzaninja@instagram
+        </p>
+        <p class="column">
+            Copyright @ 2022 Ninja Pizza (pty) limited <br>
+            All rights reserved
+        </p>
+    </footer>
 
 </body>
 
