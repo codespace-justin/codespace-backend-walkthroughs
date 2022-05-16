1449d96d14e51a684504e6389ff11370b94333d9
Justin Scott Jenecke <93656319+codespace-justin@users.noreply.github.com>
2022-05-16 13:14:08 +0200

    Update index.php

--- 0-lecture/index.php
+++ 0-lecture/index.php
@@ -3,191 +3,83 @@
 
 <?php
 
-        # - Titles -
-        $title = 'CodeSpace App Factory';
-        $subTitle = 'Developing the digital skills, coding capabilities and workplace readiness of young graduates.';
-        $productsTitle = 'What we can build';
+    $choice = $_POST['submit'];
 
-        # - Products -
-        $product1 = 'Creating Management Systems';
-        $product2 = 'Building Websites';
-        $product3 = 'Developing 3rd party API\'s';
+    $dataStore = [];
 
-             # - Metrics-
-            $metricsTitle = "Why work with us?";
-            $students = 9;
-            $activeProjects = 2;
-            $studentVacancies = false;
-            $projectVacancies = false;
-            $placedStudents = 30;
+    // creates new
+    function create($id, $flavorInput) {
 
-        # - Contact Info -
-        $footerTitle = 'Get in touch:';
-        $footerSubTitle = "Want to join our programme or place an order for some software?"
-                        . "Then fill in the form below and we will get back to ASAP!";   
-        $email = 'factory@codespace.co.za';
-        $telephone = '0' . 219063454;
-        $address = "6 Beach Rd, Woodstock, Cape Town, 8000";
+        $newDonut = [
+            "id" => $id,
+            "flavour" => $flavorInput
+        ];
 
+        array_push($newDonut);
+    }
+
+    // search specific donut
+    function read($id) {
+
+    }
+
+    // updates value fo donut
+    function update($id, $newDonut) {
+
+    }
+
+    // delete donut by id
+    function delete($id) {
+
+    }
+
+    switch ($choice) {
+        case 'create':
+            create();
+            break;
+        
+        default:
+            # code...
+            break;
+    }
+    
 ?>
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>
-        <?php
-            echo $title;
-        ?>
-    </title>
-    <link rel="stylesheet" href="./style.css">
+    <title>Deez Donuts</title>
 </head>
 <body>
 
-        <header class="header">
-            <h1>
-                <?php echo " $title "; ?>
-            </h1>
-            <h2>
-                <?php echo " $subTitle "; ?>
-            </h2>
-        </header>
+    <h2>
+        List of Donuts
+    </h2>
 
-        <br>
-        <br>
+    <ul>
+        <?php
+        for ($i=0 ; $i < count($dataStore); $i++) { 
+            echo "<li>" . $dataStore[$i] . "</li>";
+        }
+        ?>
+        
+    </ul>
 
-        <section>
-            
-            <h2>
-                <?php echo " $productsTitle "; ?>
-            </h2>
-
-            <div class="box">
-                <?php echo "<a href='#'> <b> $product1 </b></a> <br><br>"; ?>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque saepe odio ex beatae nulla cum 
-                accusantium quo ipsa blanditiis at velit, earum nostrum. Saepe assumenda ipsam velit, doloribus
-                obcaecati reprehenderit?
-            </div>
-
-            <div class="box">
-                <a href='#'> 
-                    <b> <?php echo "$product2";?> </b>
-                </a> 
-                <br><br>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque saepe odio ex beatae nulla cum 
-                accusantium quo ipsa blanditiis at velit, earum nostrum. Saepe assumenda ipsam velit, doloribus
-                obcaecati reprehenderit?
-            </div>
-
-            <div class="box">
-                <?php echo "<a href='#'> <b> $product3 </b></a> <br><br>"; ?>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque saepe odio ex beatae nulla cum 
-                accusantium quo ipsa blanditiis at velit, earum nostrum. Saepe assumenda ipsam velit, doloribus
-                obcaecati reprehenderit?
-            </div>
-
-        </section>
-
-        <br>
-        <br>
-            <!-- Metrics Section -->
-            <section>
-
-                <h2>
-                    <?php echo " $metricsTitle "; ?>
-                </h2>
-
-                <div class="box">
-                    <ul>
-                        <li>
-                            Active students working under us : <b> <?php echo " $students  "; ?> </b>
-                        </li>
-                        <li>
-                            Number of projects we are currently undertaking : <b> <?php echo " $activeProjects "; ?> </b>
-                        </li>
-                        <li>
-                            Graduates who are currently working in the industry : <b> <?php echo " $placedStudents"; ?> </b>
-                        </li>
-                    </ul>
-
-                    <div class="box" style="text-align: center;">
-                    <?php
-                        # Student Call to Action
-                        if ($studentVacancies) {
-                            echo "
-                            <button class='call-to-action-btn'>
-                                Enroll as a Student Now
-                            </button>
-                            ";
-                        } else {
-                            echo "
-                            <button disabled>
-                                Applications Full..
-                            </button>
-                            ";
-                        }
-
-                        # Client Call to Action
-                        if ($projectVacancies) {
-                            echo "
-                            <button class='call-to-action-btn'>
-                                Enquire about a project
-                            </button>
-                            ";
-                        } else {
-                            echo "
-                            <button disabled>
-                                Projects Full..
-                            </button>
-                            ";
-                        }
-                    ?>
-                    </div>
-
-                </div>
-
-            </section>
-
+    <div>
+        <h3 style="text-align: center;">
+            Create new Donut
+        </h3>
+        <form action="#" method="post">
+            <label for="">ID</label>
+            <input type="number" name="id">
             <br>
-        <br>
-
-        <!---------------------------- Footer ---------------------------->
-        <footer class="footer">
-
-            <h2>
-                <?php echo $footerTitle; ?>
-            </h2>
-            <h3>
-                <?php echo $footerSubTitle; ?>
-            </h3>
-
-            <form action="#" method="get" class="form">
-                <input type="text" name='firstname' placeholder="name" required >
-                <input type="text" placeholder="lastname" required >
-                <input type="email" name='email' placeholder="email address" required >
-                <select name="prospect">
-                    <option value="student">Student Application</option>
-                    <option value="client">Project Application</option>
-                    <option value="enquiry">General Enquiry</option>
-                </select>
-                <input type="submit" name="submit" value="Submit" class="call-to-action-btn">
-            </form>
-
-            <div class="contact">
-                <h3> 
-                    Email : <?php echo $email; ?> 
-                </h3>
-                <h3> 
-                    Tel : <?php echo $telephone; ?> 
-                </h3>
-                <h3> 
-                    Address : <?php echo $address; ?> 
-                </h3>
-            </div>
-
-        </footer>
-        <!---------------------------- Footer ---------------------------->
-
+            <label for="">Flavor</label>
+            <input type="text" name="flavor">
+            <br>
+            <input type="submit" value="create" name="submit">
+        </form>
+    </div>
 
 </body>
 </html>
